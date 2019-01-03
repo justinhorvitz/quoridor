be42c4db4bcb45570d8f7a6f6b4eb233e11b70d4
Justin Horvitz <jhorvitz@google.com>
2019-01-03 10:23:22 -0500

    Fix row/column mixup and add diagram

--- java/sublimedisruptors/quoridor/Vertex.java
+++ java/sublimedisruptors/quoridor/Vertex.java
@@ -6,11 +6,37 @@ import com.google.common.collect.ImmutableList;
 /**
  * The intersection of a distinct row and column on a quoridor board.
  *
- * <p>Rows are denoted by a lower case character beginning with {@code a} on player one's side of
- * the board and increasing toward player 2's side.
+ * <p>Columns are denoted by a lower case character beginning with {@code a} for the leftmost
+ * column (from player one's perspective) and increasing toward the right.
  *
- * <p>Columns are denoted by an integer beginning with {@code 1} for the leftmost column (from
- * player one's perspective) and increasing toward the right.
+ * <p>Rows are denoted by an integer beginning with {@code 1} from player two's side and increasing
+ * toward player one's side.
+ *
+ * <p>A typical 9x9 quoridor board is thus represented as follows:
+ *
+ *                              Player 2
+ *                 a   b   c   d   e   f   g   h   i   j
+ *               -----------------------------------------
+ *             1 |   |   |   |   |   |   |   |   |   |   | 1
+ *               -----------------------------------------
+ *             2 |   |   |   |   |   |   |   |   |   |   | 2
+ *               -----------------------------------------
+ *             3 |   |   |   |   |   |   |   |   |   |   | 3
+ *               -----------------------------------------
+ *             4 |   |   |   |   |   |   |   |   |   |   | 4
+ *               -----------------------------------------
+ *             5 |   |   |   |   |   |   |   |   |   |   | 5
+ *               -----------------------------------------
+ *             6 |   |   |   |   |   |   |   |   |   |   | 6
+ *               -----------------------------------------
+ *             7 |   |   |   |   |   |   |   |   |   |   | 7
+ *               -----------------------------------------
+ *             8 |   |   |   |   |   |   |   |   |   |   | 8
+ *               -----------------------------------------
+ *             9 |   |   |   |   |   |   |   |   |   |   | 9
+ *               -----------------------------------------
+ *                 a   b   c   d   e   f   g   h   i   j
+ *                              Player 1
  *
  * <p>It is legal to construct a vertex instance with an out-of-bounds row or column (e.g.,
  * a column less than {@code a} or a row less than {@code 1}).
