956d8920dcd3529f922299b53b6eb1ccb78acdd4
Justin Horvitz <jhorvitz@google.com>
2019-01-05 23:29:57 -0500

    Settings

---
+++ java/sublimedisruptors/quoridor/QuoridorSettings.java
@@ -0,0 +1,134 @@
+package sublimedisruptors.quoridor;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Configurable settings for a game of Quoridor.
+ *
+ * <p>Default settings may be obtained by calling {@link #defaultTwoPlayer} or {@link
+ * #defaultFourPlayer}.
+ *
+ * <p>Custom settings may be constructed via the {@link Builder}. Settings are validated when
+ * calling {@link Builder#build}. See documentation for each of the builder's setter methods for
+ * details on what is required for validity.
+ */
+@AutoValue
+public abstract class QuoridorSettings {
+
+  /** Creates default settings for a two-player game. */
+  public static QuoridorSettings defaultTwoPlayer() {
+    return builderWithCommonDefaults()
+        .setPlayers(Player.PLAYER1, Player.PLAYER2)
+        .setWallsPerPlayer(8)
+        .build();
+  }
+
+  /** Creates default settings for a four-player game. */
+  public static QuoridorSettings defaultFourPlayer() {
+    return builderWithCommonDefaults()
+        .setPlayers(Player.PLAYER1, Player.PLAYER2, Player.PLAYER3, Player.PLAYER4)
+        .setWallsPerPlayer(4)
+        .build();
+  }
+
+  private static Builder builderWithCommonDefaults() {
+    return builder().setBoardSize(9).setWallSize(2);
+  }
+
+  public static Builder builder() {
+    return new AutoValue_QuoridorSettings.Builder();
+  }
+
+  /** The number of rows and columns in the board. */
+  public abstract int boardSize();
+
+  /**
+   * The players participating in the game.
+   *
+   * <p>The order of this list represents the order in which players will take turns making moves.
+   */
+  public abstract ImmutableList<Player> players();
+
+  /** The number of {@linkplain Wall walls} each player has at the beginning of the game. */
+  public abstract int wallsPerPlayer();
+
+  /** The size of each {@link Wall}, in terms of the number of {@linkplain Edge edges} it covers. */
+  public abstract int wallSize();
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+
+    /**
+     * Sets the {@linkplain #boardSize size} of the board.
+     *
+     * <p>The size must be at least 3, and it must be odd so that starting positions can be in a
+     * middle column or row.
+     */
+    public abstract Builder setBoardSize(int boardSize);
+
+    /**
+     * Sets the {@linkplain #players} participating in the game, in the order in which they will
+     * take turns.
+     *
+     * <p>Quoridor supports 2-4 players. The {@code players} parameter must not contain {@code
+     * null}, nor duplicate players.
+     */
+    public abstract Builder setPlayers(Player... players);
+
+    /**
+     * Sets the number of walls allocated to each player at the beginning of the game.
+     *
+     * <p>This value must not be negative. It may be 0, in which case the game will be played
+     * without walls.
+     */
+    public abstract Builder setWallsPerPlayer(int wallsPerPlayer);
+
+    /**
+     * Sets the {@linkplain #wallSize size} of each {@link Wall}.
+     *
+     * <p>This value must be at least 1.
+     */
+    public abstract Builder setWallSize(int wallSize);
+
+    /**
+     * Builds the settings.
+     *
+     * <p>This method performs several validity checks. It throws {@link IllegalStateException} if
+     * any setting is invalid.
+     */
+    public final QuoridorSettings build() {
+      QuoridorSettings settings = autoBuild();
+      validateBoardSize(settings.boardSize());
+      validatePlayers(settings.players());
+      validateWallsPerPlayer(settings.wallsPerPlayer());
+      validateWallSize(settings.wallSize());
+      return settings;
+    }
+
+    private static void validateBoardSize(int boardSize) {
+      checkState(boardSize >= 3, "Board size must be at least 3, got %s", boardSize);
+      checkState(boardSize % 2 != 0, "Board size must be odd, got %s", boardSize);
+    }
+
+    private static void validatePlayers(ImmutableList<Player> players) {
+      checkState(players.size() >= 2, "Need at least two players, got %s", players);
+      checkState(players.size() <= 4, "Maximum of four players, got %s", players);
+      checkState(
+          players.stream().distinct().count() == players.size(), "Duplicate player: %s", players);
+    }
+
+    private static void validateWallsPerPlayer(int wallsPerPlayer) {
+      checkState(
+          wallsPerPlayer >= 0, "Walls per player must be non-negative, got %s", wallsPerPlayer);
+    }
+
+    private static void validateWallSize(int wallSize) {
+      checkState(wallSize >= 1, "Wall size must be at least 1, got %s", wallSize);
+    }
+
+    abstract QuoridorSettings autoBuild();
+  }
+}
---
+++ javatests/sublimedisruptors/quoridor/QuoridorSettingsTest.java
@@ -0,0 +1,74 @@
+package sublimedisruptors.quoridor;
+
+import static com.google.common.truth.Truth.assertThat;
+import static sublimedisruptors.quoridor.testing.TestUtils.assertThrows;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link QuoridorSettings}. */
+@RunWith(JUnit4.class)
+public final class QuoridorSettingsTest {
+
+  private final QuoridorSettings.Builder customSettings =
+      QuoridorSettings.builder()
+          .setBoardSize(3)
+          .setPlayers(Player.PLAYER1, Player.PLAYER2)
+          .setWallSize(2)
+          .setWallsPerPlayer(1);
+
+  @Test
+  public void defaultTwoPlayer_valid() {
+    QuoridorSettings settings = QuoridorSettings.defaultTwoPlayer();
+    assertThat(settings).isNotNull();
+  }
+
+  @Test
+  public void defaultFourPlayer_valid() {
+    QuoridorSettings settings = QuoridorSettings.defaultFourPlayer();
+    assertThat(settings).isNotNull();
+  }
+
+  @Test
+  public void customSettings_valid() {
+    QuoridorSettings settings = customSettings.build();
+    assertThat(settings).isNotNull();
+  }
+
+  @Test
+  public void boardSizeTooSmall_invalid() {
+    customSettings.setBoardSize(1);
+    assertThrows(IllegalStateException.class, customSettings::build);
+  }
+
+  @Test
+  public void boardSizeEven_invalid() {
+    customSettings.setBoardSize(8);
+    assertThrows(IllegalStateException.class, customSettings::build);
+  }
+
+  @Test
+  public void tooFewPlayers_invalid() {
+    customSettings.setPlayers(Player.PLAYER1);
+    assertThrows(IllegalStateException.class, customSettings::build);
+  }
+
+  @Test
+  public void duplicatePlayer_invalid() {
+    customSettings.setPlayers(Player.PLAYER1, Player.PLAYER2, Player.PLAYER2);
+    assertThrows(IllegalStateException.class, customSettings::build);
+  }
+
+  @Test
+  public void negativeWallsPerPlayer_invalid() {
+    customSettings.setWallsPerPlayer(-1);
+    assertThrows(IllegalStateException.class, customSettings::build);
+  }
+
+  @Test
+  public void wallSizeZero_invalid() {
+    customSettings.setWallSize(0);
+    assertThrows(IllegalStateException.class, customSettings::build);
+  }
+}
